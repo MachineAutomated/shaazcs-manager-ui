25432871f9c3493a301cc10496e1dfe32b23c716
saqib4968@gmail.com <saqib4968@gmail.com>
2025-10-12 20:31:37 +0530

    Removing reference to a deleted logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React,{ useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import type { Module, SubModule } from "./components/sidebar";
 import { Splitter, SplitterPanel } from 'primereact/splitter';
