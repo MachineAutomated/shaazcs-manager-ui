7218192b2e3fea7d8357d9e38e66326613c63de6
saqib4968@gmail.com <saqib4968@gmail.com>
2025-10-12 13:56:27 +0530

    Adding primereact components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Sidebar from "./components/sidebar";
 import type { Module, SubModule } from "./components/sidebar";
 import Content from "./components/Content";
+import { Splitter, SplitterPanel } from 'primereact/splitter';
 
 const App: React.FC = () => {
   const [selectedModule, setSelectedModule] = useState<Module>(null);
@@ -9,6 +10,8 @@ const App: React.FC = () => {
 
   return (
     <div style={{ display: "flex", height: "100vh" }}>
+    <Splitter >
+    <SplitterPanel className="flex align-items-center justify-content-center" size={25} minSize={10}>
       <Sidebar
         selectedModule={selectedModule}
         selectedSubModule={selectedSubModule}
@@ -18,12 +21,16 @@ const App: React.FC = () => {
         }}
         onSubModuleSelect={setSelectedSubModule}
       />
+    </SplitterPanel>
+    <SplitterPanel className="flex align-items-center justify-content-center" size={75}>
       <div style={{ flex: 1, padding: "20px", overflowY: "auto" }}>
         <Content
           selectedModule={selectedModule}
           selectedSubModule={selectedSubModule}
         />
       </div>
+    </SplitterPanel>
+    </Splitter>
     </div>
   );
 };
