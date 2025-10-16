f761b297372d0f3abad36beb67820e971b705f3a
saqib4968@gmail.com <saqib4968@gmail.com>
2025-10-16 23:24:23 +0530

    #7 : Started with api setup for MonthEndSummary

--- src/api/transactionApi.ts
+++ src/api/transactionApi.ts
@@ -2,13 +2,17 @@ import axios from "axios";
 import { API_BASE_URL } from "../config";
 
 export async function saveTransaction(data: any) {
-    return axios.post(`${API_BASE_URL}/saveTransaction`, data);
-  }
+  return axios.post(`${API_BASE_URL}/saveTransaction`, data);
+}
 
 export async function getSummary() {
-    return axios.get(`${API_BASE_URL}/summary?year=2025&month=10`);
-  }
+  return axios.get(`${API_BASE_URL}/summary?year=2025&month=10`);
+}
 
 export async function getTransactionsDetialsByMonth(year: Number, month: Number) {
-    return axios.get(`${API_BASE_URL}/transactionDetails?year=${year}&month=${month}`)
-  }
+  return axios.get(`${API_BASE_URL}/transactionDetails?year=${year}&month=${month}`)
+}
+
+export async function getMonthEndSummaryForMonth(year: Number, month: Number) {
+  return axios.get(`${API_BASE_URL}/monthEnd?year=${year}&month=${month}`)
+}
