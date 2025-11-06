adcb4fce0387193880a1a13647fdaddd560ede86
saqib4968 <saqib4968@gmail.com>
2025-11-06 23:03:38 +0530

    #13 removing unsed import

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Card } from "primereact/card";
 import { Calendar } from "primereact/calendar";
 import { Button } from "primereact/button";
