104624555a1e7d2f93c5d747ea413dae63de308f
saqib4968 <saqib4968@gmail.com>
2025-11-10 00:39:46 +0530

    Fixes #11 : Registeration works successfully

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,13 +6,17 @@ import { Card } from "primereact/card";
 import { useNavigate } from "react-router-dom";
 import api from "../api/api";
 import "../styles/login.css";
+import { Dialog } from 'primereact/dialog';
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [registerUsername, setRegisterUsername] = useState<string>("");
+  const [registerPassword, setRegisterPassword] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [registering] = useState<boolean>(false);
+  const [registering, setRegistering] = useState<boolean>(false);
+  const [registerDialogVisibility, setRegisterDialogVisibility] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
 
   const handleLogin = async () => {
@@ -40,19 +44,49 @@ const LoginPage: React.FC = () => {
     }
   };
 
+  const handleRegister = async () => {
+    setLoading(true);
+    setError("");
+    setRegistering(true);
+
+    try {
+
+      const response = await api.post("/register", {
+        username,
+        password,
+      });
+
+      if (response.status !== 200) {
+        throw new Error("Registration failed");
+      }
+
+      const data = response.data;
+      const clientName = data["clientName"];
+
+      setRegisterDialogVisibility(false);
+      alert(`Registration successful! ${clientName} can now log in after closing this alert.`);
+
+    } catch (err) {
+      console.error("Registration failed:", err);
+      setError("Registration error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="login-page" style={{ height:'400px', width:'500px', alignContent: 'center'}}>
+    <div className="login-page" style={{ height: '400px', width: '500px', alignContent: 'center' }}>
       <Card title="Login" className="login-card">
         <div className="login-row">
           <label htmlFor="username" className="input-label">
             Username
           </label>
           <InputText
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Enter username"
-              style={{ width: '63%'}}
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter username"
+            style={{ width: '63%' }}
           />
         </div>
         <div className="login-row">
@@ -60,13 +94,13 @@ const LoginPage: React.FC = () => {
             Password
           </label>
           <Password
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              feedback={false}
-              toggleMask
-              placeholder="Enter password"
-              style={{ width: '30%'}}
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            feedback={false}
+            toggleMask
+            placeholder="Enter password"
+            style={{ width: '30%' }}
           />
         </div>
         <div className="error-row">
@@ -83,18 +117,55 @@ const LoginPage: React.FC = () => {
               style={{ marginTop: '10px', marginRight: '5px' }}
             />
           </div>
-          
+
           <div className="action-col">
             <Button
-              label={registering ? "Registering..." : "Register"}
+              label={registerDialogVisibility ? "Registering..." : "Register"}
               icon="pi pi-register"
+              onClick={() => { setRegisterDialogVisibility(true)}}
               className="p-button-rounded p-button-outlined register-btn"
               style={{ marginTop: '10px' }}
             />
+            <Dialog header="Register with new user and password" visible={registerDialogVisibility} style={{ width: '50vw' }} onHide={() => { if (!registerDialogVisibility) return; setRegisterDialogVisibility(false); }}>
+              <div >
+                {/* <label htmlFor="username" className="input-label">
+                  Username
+                </label> */}
+                <InputText
+                  id="register_username"
+                  value={registerUsername}
+                  onChange={(e) => setRegisterUsername(e.target.value)}
+                  placeholder="Enter username"
+                  
+                />
+              {/* </div>
+              <div className="login-row"> */}
+                {/* <label htmlFor="password" className="input-label" >
+                  Password
+                </label> */}
+                <Password
+                  id="register_password"
+                  value={registerPassword}
+                  onChange={(e) => setRegisterPassword(e.target.value)}
+                  feedback={false}
+                  toggleMask
+                  placeholder="Enter password"
+                  style={{ marginLeft: '10px' }}
+                />
+                <Button
+                  label={registering ? "Registering..." : "Register"}
+                  icon="pi pi-register"
+                  onClick={handleRegister}
+                  className="p-button-rounded p-button-outlined register-btn"
+                  style={{ marginLeft: '10px' }}
+                />
+              </div>
+            </Dialog>
+
           </div>
         </div>
 
-        
+
       </Card>
     </div>
   );
