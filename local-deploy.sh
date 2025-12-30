5c8b740adfb88eb9c3806d05f113a11911b816ee
saqib4968 <saqib4968@gmail.com>
2025-12-30 23:57:04 +0530

    [skip] #24

--- local-deploy.sh
+++ local-deploy.sh
@@ -2,3 +2,40 @@
 docker image inspect ghcr.io/machineautomated/finance-mgr:1.0.0 \
   --format='{{.Created}}'
 
+
+#Identify remote image timestamp (GitHub Container Registry)
+export GITHUB_USER=saqib4968
+
+TOKEN=$(curl -s -u $GITHUB_USER:$GITHUB_PERSONAL_PKG_RW_TOKEN \
+"https://ghcr.io/token?service=ghcr.io&scope=repository:machineautomated/shaazcs-manager-ui:pull" \
+| jq -r .token)
+
+
+
+
+
+MANIFEST=$(curl -H "Authorization: Bearer $TOKEN" \
+https://ghcr.io/v2/machineautomated/shaazcs-manager-ui/manifests/1.0.0)
+
+
+echo "$MANIFEST" | jq .schemaVersion
+
+CONFIG_DIGEST=$(echo "$MANIFEST" | jq -r '.config.digest')
+
+
+
+CONFIG=$(curl -s \
+  -u "$GITHUB_USER:$GITHUB_PERSONAL_PKG_RW_TOKEN" \
+  https://ghcr.io/v2/machineautomated/shaazcs-manager-ui/blobs/$CONFIG_DIGEST)
+
+
+REGISTRY_TOKEN=$(curl -s \
+  -u "$GITHUB_USER:$GITHUB_PERSONAL_PKG_RW_TOKEN" \
+  "https://ghcr.io/token?service=ghcr.io&scope=repository:machineautomated/shaazcs-manager-ui:pull" \
+  | jq -r '.token')
+
+
+CONFIG=$(curl -s -L \
+  -H "Authorization: Bearer $REGISTRY_TOKEN" \
+  -H "Accept: application/vnd.docker.container.image.v1+json" \
+  https://ghcr.io/v2/machineautomated/shaazcs-manager-ui/blobs/$CONFIG_DIGEST)
