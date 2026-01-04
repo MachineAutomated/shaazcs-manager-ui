0d1a6ccbc739f32bb854a6683b9d3bed74a26e1e
saqib4968 <saqib4968@gmail.com>
2026-01-04 13:41:22 +0530

    skip ci

--- local-deploy-poc.sh
+++ local-deploy-poc.sh
@@ -12,8 +12,6 @@ TOKEN=$(curl -s -u $GITHUB_USER:$GITHUB_PERSONAL_PKG_RW_TOKEN \
 
 
 
-
-
 MANIFEST=$(curl -H "Authorization: Bearer $TOKEN" \
 https://ghcr.io/v2/machineautomated/shaazcs-manager-ui/manifests/1.0.0)
 
@@ -24,18 +22,10 @@ CONFIG_DIGEST=$(echo "$MANIFEST" | jq -r '.config.digest')
 
 
 
-CONFIG=$(curl -s \
-  -u "$GITHUB_USER:$GITHUB_PERSONAL_PKG_RW_TOKEN" \
-  https://ghcr.io/v2/machineautomated/shaazcs-manager-ui/blobs/$CONFIG_DIGEST)
-
-
-REGISTRY_TOKEN=$(curl -s \
-  -u "$GITHUB_USER:$GITHUB_PERSONAL_PKG_RW_TOKEN" \
-  "https://ghcr.io/token?service=ghcr.io&scope=repository:machineautomated/shaazcs-manager-ui:pull" \
-  | jq -r '.token')
-
-
-CONFIG=$(curl -s -L \
-  -H "Authorization: Bearer $REGISTRY_TOKEN" \
+curl -s -L \
+  -H "Authorization: Bearer $TOKEN" \
   -H "Accept: application/vnd.docker.container.image.v1+json" \
-  https://ghcr.io/v2/machineautomated/shaazcs-manager-ui/blobs/$CONFIG_DIGEST)
+  https://ghcr.io/v2/machineautomated/shaazcs-manager-ui/blobs/$CONFIG_DIGEST \
+| jq -r '.created'
+
+
